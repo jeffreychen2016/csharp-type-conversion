89ae2efa525ac73d8491d1af7541721320151cbb
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-08-30 20:23:00 -0500

    type conversion code along

--- TypeConversion/Program.cs
+++ TypeConversion/Program.cs
@@ -6,7 +6,33 @@ namespace TypeConversion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*------------------------------Type Conversion-----------------------------*/
+
+            //int i = 1;
+            //byte b = (byte)i;  -- (byte)  is called casting
+            //Console.WriteLine(b);
+            //Console.ReadLine();
+
+            /* 
+                when convert large data type to smaller data type 
+                the data may get lost
+                it is called overflow
+            */
+
+            try
+            {
+                var number = "1234";
+                byte i = Convert.ToByte(number);
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to byte");
+                Console.ReadLine();
+            }
+
+
         }
     }
 }
